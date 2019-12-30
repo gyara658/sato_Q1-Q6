3c356355257025a929cd96ba7207bab6c1a47003
gyara658 <49307752+gyara658@users.noreply.github.com>
2019-12-30 21:06:30 +0900

    re33.34

--- Q33.rb
+++ Q33.rb
@@ -1,8 +1,19 @@
 date1 ={name: "あじー", age: 32, address: "札幌", tell: "090-000-000"}
 date2 ={name: "あじー", age: 32, address: "札幌"}
 master = [:name,:age,:address,:tell]
+#date1 = { age: 32, name: "あじー", address: "札幌", tell: "090-000-000" }
+date3 ={ age: 32, name: "あじー", address: "札幌", tell: "090-000-000" }
+date4 ={age: 32,name: "あじー", address: "札幌"}
 
-date1_key = date1.keys
-date2_key = date2.keys
-p date1_key==master
-p date2_key==master
+date1_key = date1.keys.sort
+date2_key = date2.keys.sort
+date3_key = date3.keys.sort
+date4_key = date4.keys.sort
+master_sort = master.sort
+#p date1_key
+#p date3_key
+#p mas
+p date1_key==master_sort
+p date2_key==master_sort
+p date3_key==master_sort
+p date4_key==master_sort
--- Q34.rb
+++ Q34.rb
@@ -1,8 +1,13 @@
 programming_language = ["ruby", "php", "python", "javascript"]
 
-p programming_language.map!{|word|word.capitalize}
-upper_case_programming_language= programming_language.map{|word|word.upcase}
-p upper_case_programming_language
+#p programming_language.map!{|word|word.capitalize}
+#upper_case_programming_language= programming_language.map{|word|word.upcase}
+#p upper_case_programming_language
 
 #["Ruby", "Php", "Python", "Javascript"]
 #["RUBY", "PHP", "PYTHON", "JAVASCRIPT"]
+
+
+p programming_language.map!(&:capitalize)
+upper_case_programming_language= programming_language.map(&:upcase)
+p upper_case_programming_language
