f1bf505e1b1dff0e8af3ea33d895be56607ebf7f
gyara658 <49307752+gyara658@users.noreply.github.com>
2019-12-30 16:37:07 +0900

    re30,32

--- Q32.rb
+++ Q32.rb
@@ -16,29 +16,47 @@ class Zoo
     @senior = params[:senior]
   end
 
+
+
   def check_entry_fee(user)
-    if user.age < 6 then
-      puts "#{user.name}は#{user.age}才なので#{baby}円です"
-    elsif  user.age < 13 then
-      puts "#{user.name}は#{user.age}才なので#{child}円です"
-    elsif user.age < 65 then
-      puts "#{user.name}は#{user.age}才なので#{adult}円です"
-    elsif user.age < 121 then
-      puts "#{user.name}は#{user.age}才なので#{senior}円です"
+    price = case user.age
+    when 0..5
+      price = 50
+    when 6..12
+      price = 120
+    when 13..64
+      price = 640
+    when 65..120
+      price = 1200
     else
-      puts "無効な値です。"
+      return
+      #puts "無効な値です"
+    end
+
+
+    case user.age
+    when  0..5 then
+      puts "#{user.name}は#{user.age}才なので#{price}円です"
+    when 6..12 then
+      puts "#{user.name}は#{user.age}才なので#{price}円です"
+    when 13..64 then
+      puts "#{user.name}は#{user.age}才なので#{price}円です"
+    when 65..120 then
+      puts "#{user.name}は#{user.age}才なので#{price}円です"
+    #else
+      #puts "無効な値です。"
     end
   end
 end
 
-user1 = User.new(name:"あじー",age:35)
+user1 = User.new(name:"あじー",age:64)
 user2 = User.new(name:"ゆたボン",age:120)
 user3 = User.new(name:"はいさい",age:3)
 user4 = User.new(name:"ヤンくる",age:11)
 user5 = User.new(name:"シーサー",age:145)
 
-
-zoo = Zoo.new(name:"上野動物園",baby:50,child:120,adult:640,senior:1200)
+zoo = Zoo.new(name:"")
+#zoo = Zoo.new(name:"上野動物園",baby:50,child:120,adult:640,senior:1200)
 zoo.check_entry_fee(user1)
 zoo.check_entry_fee(user2)
 zoo.check_entry_fee(user3)
